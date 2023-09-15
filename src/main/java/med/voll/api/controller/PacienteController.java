4f191ce24c0cfd659daa8fae18d1bc2d6b541c24
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-15 08:58:56 -0300

    feat: Implements listar method with pagination and ordenation

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -6,6 +6,9 @@ import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +28,7 @@ public class PacienteController {
     }
 
     @GetMapping
-    public List<DadosListagemPaciente> listar() {
-        return repository.findAll().stream().map(DadosListagemPaciente::new).toList();
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size=10, sort={"nome"}) Pageable pageable) {
+        return repository.findAll(pageable).map(DadosListagemPaciente::new);
     }
 }
