6ecc53aa78d21b06913131d0e18efe164ce4257d
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-20 14:17:53 -0300

    feat: add new field ativo

--- src/main/java/med/voll/api/paciente/DadosAtualizacaoPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosAtualizacaoPaciente.java
@@ -3,10 +3,13 @@ package med.voll.api.paciente;
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.endereco.DadosEndereco;
 
+import java.io.StringBufferInputStream;
+
 public record DadosAtualizacaoPaciente(
         @NotNull
         Long id,
         String nome,
         String telefone,
+        Boolean ativo,
         DadosEndereco endereco) {
 }
