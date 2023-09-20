f3f660a7535b0681ccd9b0969dd385d9f2492843
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-20 15:02:14 -0300

    feat: implements new method on class to handle erros

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -1,15 +1,33 @@
 package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @RestControllerAdvice
 public class TratadorDeErros {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity trataError404() {
+    public ResponseEntity tratarError404() {
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErrorValidacao::new).toList());
+    }
+
+    private record DadosErrorValidacao(String campo, String message) {
+        DadosErrorValidacao(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
 }
